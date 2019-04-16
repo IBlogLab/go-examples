f00db200258a7c7e0eb873a94ac45288366d7ead
xushaoxiao <xushaoxiao@flashhold.com>
2019-04-16 23:52:15 +0800

    add constant example

--- constants/constants_test.go
+++ constants/constants_test.go
@@ -1,10 +1,22 @@
 package constants
 
-import "testing"
+import (
+	"fmt"
+	"math"
+	"testing"
+)
 
-//
+// 声明常量
 const s string = "constant"
 
 func TestConstants(t *testing.T) {
+	fmt.Println(s)
 
+	const n = 500000000
+
+	const d = 3e20 / n
+	fmt.Println(d)
+
+	fmt.Println(int64(n))
+	fmt.Println(math.Sin(n))
 }

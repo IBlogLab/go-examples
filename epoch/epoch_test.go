794db72ffd33d447b5e2422e651aadaf1705b3b9
xushaoxiao <xushaoxiao@flashhold.com>
2019-04-30 17:02:20 +0800

    add epoch example

---
+++ epoch/epoch_test.go
@@ -0,0 +1,22 @@
+package epoch_test
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestEpoch(t *testing.T) {
+	now := time.Now()
+	secs := now.Unix()
+	nanos := now.UnixNano()
+	fmt.Println(now)
+
+	millis := nanos / 1000000
+	fmt.Println(secs)
+	fmt.Println(millis)
+	fmt.Println(nanos)
+
+	fmt.Println(time.Unix(secs, 0))
+	fmt.Println(time.Unix(0, nanos))
+}
